64adff0c389623b02c4c8c373181518b7b14d517
Cléber Barros <cleber.barros@inep.gov.br>
2024-08-24 22:34:48 -0300

    ajuste DTO de venda para incluir campo valor

--- backend/api_service/src/main/java/tjrj/dto/VendaDTO.java
+++ backend/api_service/src/main/java/tjrj/dto/VendaDTO.java
@@ -2,10 +2,13 @@ package tjrj.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class VendaDTO {
 
     private Long id;
     private String descricao;
+    private BigDecimal valor;
 }
 
